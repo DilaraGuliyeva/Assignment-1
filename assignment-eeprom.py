4dff87b694b023514b9d64143926bd4dd5acc165
Dilara <dilara@Dilaras-MacBook-Air.local>
2025-08-18 02:03:47 -0400

    New line $ isaresi ile evezledimki 4 wheel-i ayira bilim. Bura qeder list icine 4 ayri wheel yigilib. Idx 0- wheel1,Idx 1 - wheel 2 ve.s  List icinde list.

--- assignment-eeprom.py
+++ assignment-eeprom.py
@@ -1,35 +1,69 @@
 try:
     with open ("/Users/dilara/Desktop/Dilara/Assignment1/colleague-file.log","r") as file:
         content = file.read()
-        #print(content)
+        print(content)
+        # if "\n\n" in content:
+            
+        # # Perform action, e.g., process paragraphs
+        #     content = content.split("\n\n")
+        #     print(content)
 
 except FileNotFoundError:
     print("Error: The specified file was not found.")
 
+content = content.replace("\n", " $ ")
 
-content = content.replace('\n',' ')
-    
-#number_str_list = ["0","1","2","3","4","5","6","7","8","9"]
-
-# Stringi sadece liste cevirdik
+num_inner_wheel_list = 4
+wheels_list = [[] for _ in range(num_inner_wheel_list)]
 content_elem = ""
-content_list = []
-
+count_for_split_wheel = 0
+idx = 0
+print(content)
 
 for elem in content:
-    
-    if elem != " " :
+    if elem != " ":
 
         content_elem += elem
+
+
+    if elem == " " and content_elem != "" and content_elem != "$":
+        wheels_list[idx].append(content_elem)
+        count_for_split_wheel = 0
+        content_elem = ""
+        
+
+    elif content_elem == "$":
+        
+        count_for_split_wheel += 1
+        content_elem = ""
+        
+        if count_for_split_wheel >= 2:
+            idx +=1
+            count_for_split_wheel = 0
+            
+        
+
+
+print(wheels_list)
+#Stringi sadece liste cevirdik
+# content_elem = ""
+# content_list = []
+
+
+# for elem in content:
+    
+#     if elem != " " :
+
+#         content_elem += elem
     
 
-    if elem == " " and content_elem != "":
-       content_list.append(content_elem)
-       content_elem = ""
+#     if elem == " " and content_elem != "":
+#        content_list.append(content_elem)
+#        content_elem = ""
 
-content_list.append(content_elem)        
+# content_list.append(content_elem)        
 
-print(content_list)
+# print(content_list)
 # liste cevirme bitdi!
 
 
