324ff6f92376120c69916e1f1ec390e82d7eac50
Dilara <dilara@Dilaras-MacBook-Air.local>
2025-08-19 18:37:53 -0400

    Kodu classa cevirdim. Yeni class yaradib movcud kodlarimi ona uygunlasdirdim. Hal hazirda class cagirilanda wheels listi ve icinde her wheel ayri list kimi yigilir.

--- assignment-eeprom.py
+++ assignment-eeprom.py
@@ -1,87 +1,92 @@
-try:
-    with open ("/Users/dilara/Desktop/Dilara/Assignment1/colleague-file.log","r") as file:
-        content = file.read()
-        print(content)
-        # if "\n\n" in content:
-            
-        # # Perform action, e.g., process paragraphs
-        #     content = content.split("\n\n")
-        #     print(content)
+# try:
+#     with open ("/Users/dilara/Desktop/Dilara/Assignment1/colleague-file.log","r") as file:
+#         content = file.read()
+#         print(content)
+       
+# except FileNotFoundError:
+#     print("Error: The specified file was not found.")
 
-except FileNotFoundError:
-    print("Error: The specified file was not found.")
-
-content = content.replace("\n", " $ ")
-
-num_inner_wheel_list = 4
-wheels_list = [[] for _ in range(num_inner_wheel_list)]
-content_elem = ""
-count_for_split_wheel = 0
-idx = 0
-print(content)
-
-for elem in content:
-    if elem != " ":
-
-        content_elem += elem
-
-
-    if elem == " " and content_elem != "" and content_elem != "$":
-        wheels_list[idx].append(content_elem)
-        count_for_split_wheel = 0
-        content_elem = ""
-        
-
-    elif content_elem == "$":
-        
-        count_for_split_wheel += 1
-        content_elem = ""
-        
-        if count_for_split_wheel >= 2:
-            idx +=1
-            count_for_split_wheel = 0
-            
-        
-
-
-print(wheels_list)
-#Stringi sadece liste cevirdik
-# content_elem = ""
-# content_list = []
-
-
-# for elem in content:
-    
-#     if elem != " " :
-
-#         content_elem += elem
-    
-
-#     if elem == " " and content_elem != "":
-#        content_list.append(content_elem)
-#        content_elem = ""
-
-# content_list.append(content_elem)        
-
-# print(content_list)
-# liste cevirme bitdi!
-
+# content = content.replace("\n", " $ ")
 
 # num_inner_wheel_list = 4
 # wheels_list = [[] for _ in range(num_inner_wheel_list)]
-# wheel_list_idx = 0
-# wheel_size_counter = 9
-# checksum = 0
+# content_elem = ""
+# count_for_split_wheel = 0
+# idx = 0
+# print(content)
 
-# for idx in range(len(content_list)):
-#     if idx < wheel_size_counter-1:
-#         wheel_size_counter += int(content_list[idx])
+# for elem in content:
+#     if elem != " ":
+
+#         content_elem += elem
+
+
+#     if elem == " " and content_elem != "" and content_elem != "$":
+#         wheels_list[idx].append(content_elem)
+#         count_for_split_wheel = 0
+#         content_elem = ""
         
-#     elif idx <= wheel_size_counter:
-#         wheels_list.insert(wheel_list_idx,content[idx])
-    
-#     wheel_list_idx +=1
-    
 
+#     elif content_elem == "$":
+        
+#         count_for_split_wheel += 1
+#         content_elem = ""
+        
+#         if count_for_split_wheel >= 2:
+#             idx +=1
+#             count_for_split_wheel = 0
+            
+# # Bura qeder ancaq content ayrilib.
+
+# checksum = 0
+# region_idx_list = []
+# for i in range(len(wheels_list)):
+#     for elem_idx in range(len(wheels_list[i])):
+#         if elem_idx <=8:
+#             region_idx_list.append(wheels_list[i][elem_idx])
+            
 # print(wheels_list)
-    
+
+
+class EEPROM:
+    def __init__(self,file_path,num_inner_wheel_list = 4):
+        self.file_path = file_path
+        self.num_inner_wheel_list = num_inner_wheel_list
+        self.wheels_list = [[] for _ in range(num_inner_wheel_list)]
+        try:
+            with open (self.file_path,"r") as file:
+                content = file.read()
+                content = content.replace("\n", " $ ")
+      
+        except FileNotFoundError:
+            print("Error: The specified file was not found.")
+        
+        
+        content_elem = ""
+        count_for_split_wheel = 0
+        idx = 0
+        
+        for elem in content:
+            if elem != " ":
+
+                content_elem += elem
+
+            if elem == " " and content_elem != "" and content_elem != "$":
+                self.wheels_list[idx].append(content_elem)
+                count_for_split_wheel = 0
+                content_elem = ""
+                
+
+            elif content_elem == "$":
+                
+                count_for_split_wheel += 1
+                content_elem = ""
+                
+                if count_for_split_wheel >= 2:
+                    idx +=1
+                    count_for_split_wheel = 0
+        self.wheels_list[idx].append(content_elem)
+        print(self.wheels_list)
+
+eeprom = EEPROM("/Users/dilara/Desktop/Dilara/Assignment1/colleague-file.log")
+print(eeprom)
